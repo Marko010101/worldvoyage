bfaeac27bed5ae177c8d1a1b86482529c8c8529c
Marko <baghashvili.marko@gmail.com>
2024-02-23 21:04:20 +0400

    Started working on a new project using Vite, CSS modules, and React Router.

---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav.jsx";
+
+function Pricingpage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Pricing</h1>
+    </div>
+  );
+}
+
+export default Pricingpage;
