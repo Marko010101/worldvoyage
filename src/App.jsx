fbf4d764d950898d09ab91112268e406a4c0ba67
Marko <baghashvili.marko@gmail.com>
2024-02-24 19:30:59 +0400

    Built pages and components for the HomePage, Pricing, and Product with their respective routes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
